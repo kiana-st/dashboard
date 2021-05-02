e4a84d6ea460d9c82971fcdab9e0dd2a309a088e
kiana <soltany.2019@gmail.com>
2021-05-02 14:48:52 +0430

    data sidebar

--- my-app/src/components/sidebar/SidebarData.js
+++ my-app/src/components/sidebar/SidebarData.js
@@ -3,18 +3,69 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as LoIcons from 'react-icons/io';
 import * as HiIcons from 'react-icons/hi';
+import * as IoIcons from 'react-icons/io5';
 
 export const SidebarData = [
   {
-    title: 'SmileIcon',
-    icon: <FaIcons.FaGrinWink />,
-    subNav: [
+    titleSmile: 'general',
+    iconSmile: <FaIcons.FaGrinWink />,
+    iconUser: <FaIcons.FaUserCircle />,
+    iconToggle: <IoIcons.IoChevronBackSharp />,
+    iconUp: <FaIcons.FiMenu />,
+    iconLeft: <FaIcons.FiChevronLeft />,
+  },
+
+  {
+    title: 'داشبورد',
+    iconDashboard: <AiIcons.AiOutlineDashboard />,
+  },
+
+  {
+    name: 'مدیریت',
+    title1: 'اپلیکیشن ها',
+    title2: 'مدیریت',
+    path: './اپلیکیشن ها',
+    iconSetting: <LoIcons.IoMdSettings />,
+    iconTool: <AiIcons.AiFillTool />,
+    subNavApp: [
       {
-        iconDashboard: <AiIcons.AiOutlineDashboard />,
-        iconSetting: <LoIcons.IoMdSettings />,
-        iconTool: <AiIcons.AiFillTool />,
-        iconDoc: <HiIcons.HiFolder />,
-       
+        title: 'همه اپلیکیشن ها',
+        path: './همه اپلیکیشن ها/اپلیکیشن ها/اپلیکیشن ها/مدیریت',
+      },
+      {
+        title: 'اپلیکیشن های من',
+        path: './اپلیکیشن های من/اپلیکیشن ها/اپلیکیشن ها/مدیریت',
+      },
+    ],
+    subNavTool: [
+      {
+        name: ':کدهای فعال سازی',
+        title: 'پین کدهای من',
+        path: './پین کدهای من/کدهای فعال سازی/مدیریت/مدیریت',
+      },
+      {
+        name: ':سخت افزارها',
+        title: 'سخت افزارهای من',
+        path: './سخت افزارهای من/سخت افزارها/مدیریت/مدیریت/',
+      },
+    ],
+  },
+
+  {
+    name: 'گزارشات',
+    title: 'گزارشات',
+    iconDoc: <HiIcons.HiFolder />,
+    subNavDoc: [
+      {
+        name: 'پرداخت ها',
+        title: 'گزارش پرداخت ها',
+        path: './گزارش پرداخت ها/پرداخت ها/گزارشات/گزارشات',
+      },
+      {
+        name: 'کاربران',
+        title1: 'تیکت ها',
+        title2: 'پروفایل من',
+        path: './پروفایل من/کاربران/گزارشات/گزارشات',
       },
     ],
   },
