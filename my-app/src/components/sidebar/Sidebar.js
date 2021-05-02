93e8d8236bc86b19370e363433069321def5e92b
kiana <soltany.2019@gmail.com>
2021-05-02 13:12:34 +0430

    new file

---
+++ my-app/src/components/sidebar/Sidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return <div>ssss</div>;
+};
+
+export default Sidebar;
---
+++ my-app/src/components/sidebar/SidebarData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as LoIcons from 'react-icons/io';
+import * as HiIcons from 'react-icons/hi';
+
+export const SidebarData = [
+  {
+    title: 'SmileIcon',
+    icon: <FaIcons.FaGrinWink />,
+    subNav: [
+      {
+        iconDashboard: <AiIcons.AiOutlineDashboard />,
+        iconSetting: <LoIcons.IoMdSettings />,
+        iconTool: <AiIcons.AiFillTool />,
+        iconDoc: <HiIcons.HiFolder />,
+      },
+    ],
+  },
+];
