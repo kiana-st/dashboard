1ade5a72b4ff25b92476aa2edf531dd462583460
kiana <soltany.2019@gmail.com>
2021-05-03 14:20:16 +0430

    user button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      <Router>
+        <Sidebar />
+      </Router>
     </div>
   );
 }
--- my-app/src/components/sidebar/Sidebar.js
+++ my-app/src/components/sidebar/Sidebar.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './sidebar.css';
+import * as FaIcons from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  return <div>test</div>;
+  return (
+    <div>
+      <nav className="nav">
+        <navIcon to="#">
+          <div className="user">
+            <Link className="linkuser" to="#">ورود / عضویت</Link>
+            <FaIcons.FaUserCircle className="usericon" />
+          </div>
+        </navIcon>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidebar;
